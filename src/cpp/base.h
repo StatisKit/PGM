398f0284f153c02980a32c934194534f1887237b
Pierre Fernique <pfernique@gmail.com>
2017-04-27 12:02:14 +0200

    Add AutoWIG.py

---
+++ src/cpp/base.h
@@ -0,0 +1,29 @@
+#ifndef STATISKIT_PGM_PGM_H
+#define STATISKIT_PGM_PGM_H
+
+#if defined WIN32 || defined _WIN32 || defined __CYGWIN__
+  #ifdef LIBSTATISKIT_PGM
+    #ifdef __GNUC__
+      #define STATISKIT_PGM_API __attribute__ ((dllexport))
+    #else
+      #define STATISKIT_PGM_API __declspec(dllexport)
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define STATISKIT_PGM_API __attribute__ ((dllimport))
+    #else
+      #define STATISKIT_PGM_API __declspec(dllimport)
+    #endif
+  #endif
+#else
+  #if __GNUC__ >= 4
+    #define STATISKIT_PGM_API __attribute__ ((visibility ("default")))
+  #else
+    #define STATISKIT_PGM_API
+  #endif
+#endif
+
+namespace statiskit
+{
+    
+}
