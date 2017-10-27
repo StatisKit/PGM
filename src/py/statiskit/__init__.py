29a7633f555bf3bbeba6b92b8be08d41228a24bb
Pierre Fernique <pfernique@gmail.com>
2017-10-27 10:28:20 +0200

    Update

--- src/py/statiskit/__init__.py
+++ src/py/statiskit/__init__.py
@@ -1,11 +1,3 @@
-##################################################################################
-#                                                                                #
-# StatisKit-CoreThis software is distributed under the CeCILL-C license. You     #
-# should have received a copy of the legalcode along with this work. If not, see #
-# <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html>.                 #
-#                                                                                #
-##################################################################################
-
 try:
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
--- src/py/statiskit/pgm/io.py
+++ src/py/statiskit/pgm/io.py
@@ -1,11 +1,3 @@
-##################################################################################
-#                                                                                #
-# StatisKit-CoreThis software is distributed under the CeCILL-C license. You     #
-# should have received a copy of the legalcode along with this work. If not, see #
-# <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html>.                 #
-#                                                                                #
-##################################################################################
-
 __all__ = ['read_dot', 'read_gml', 'from_networkx']
 
 from optionals import networkx
